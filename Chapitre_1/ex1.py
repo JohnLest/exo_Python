39c3d9870eac058f4af02f6179a6411661c75b4c
JohnLest <j_schwickerath@hotmail.com>
2021-09-23 10:34:10 +0200

    add ex1 chap1

---
+++ Chapitre_1/ex1.py
@@ -0,0 +1,7 @@
+# Écrire un script qui permet d'afficher les 10 premiers termes (avec le calcul) 
+# de la table de multiplication par 3.
+
+i = 1
+while (i <= 10):
+    print (f"{i} * 3 = {i*3}")
+    i +=1
